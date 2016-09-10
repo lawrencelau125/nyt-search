c263b954df116d3b33674485e208ad09f2a6aced
aksm <albert.min@gmail.com>
2016-09-10 13:12:05 -0400

    Add number of page results query

--- javascript/nyt.js
+++ javascript/nyt.js
@@ -1,16 +1,18 @@
 var searchterm = "Trump"
 var beginDate = "";
 var endDate = "";
+var results = 1;
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
 		'api-key': "d6c8cdc35f93488dbaa04dd9173ea9fb",
 		// searches article body, headline, and byline
 		'q': searchterm,
-		// YYYYMMDD
+		'page': results,
 });
 
 if (beginDate !== "") {
 	url += '?' + $.param({
+		// YYYYMMDD
 		'begin_date': beginDate,
 	});
 }
