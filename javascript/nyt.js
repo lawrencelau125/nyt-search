d569d3f47c17a585b934d3e1e67f36e0df4f649d
aksm <albert.min@gmail.com>
2016-09-10 13:09:01 -0400

    Add optional date logic

--- javascript/nyt.js
+++ javascript/nyt.js
@@ -1,17 +1,25 @@
 var searchterm = "Trump"
-var beginDate = "20150101";
-var endDate = "20160101";
-var url = "https://crossorigin.me/https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var beginDate = "";
+var endDate = "";
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
 		'api-key': "d6c8cdc35f93488dbaa04dd9173ea9fb",
 		// searches article body, headline, and byline
 		'q': searchterm,
 		// YYYYMMDD
-		'begin_date': beginDate,
-		'end_date': endDate,
-		// options - newest oldest
-		'sort': "newest"
 });
+
+if (beginDate !== "") {
+	url += '?' + $.param({
+		'begin_date': beginDate,
+	});
+}
+if (endDate !== "") {
+	url += '?' + $.param({
+		'end_date': endDate,		
+	});
+}
+console.log(url);
 $.ajax({
 		url: url,
 		method: 'GET',
